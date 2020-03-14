2eaf5d7972df821afab94eb6f89fd724fb7a381b
IlyaOrluk <misfits120@mail.ru>
2020-03-14 14:48:21 +0300

    init

---
+++ src/About.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<main class='container'>
+<img src='https://media.cdn.adultswim.com/uploads/20191004/191041327281-r&m4_Hero_000000.png' alt='img'>
+    <h2>This is awesome application on awesome framework!</h2>
+</main>
+
+<style>
+    h2{
+        color: grey;
+    }
+</style>
---
+++ src/ItemList.svelte
@@ -0,0 +1,52 @@
+<script>
+	import axios from 'axios'
+    import { onMount } from 'svelte'
+    import Item from './Item.svelte'
+    import { characters } from './store.js'
+
+	onMount(() => {
+		axios.get(`https://rickandmortyapi.com/api/character/`)
+            .then((res) => {
+                characters.set(res.data.results)
+                console.log($characters)
+                })
+        
+    })
+    
+    
+</script>
+
+<!-- <main>
+<form on:submit|preventDefault={()=>{}}></form>
+</main> -->
+
+<style>
+	.characters {
+		width: 100%;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+	}
+    .character__img {
+        width: 150px;
+        height: 150px;
+    }
+</style>
+
+<h1>All characters</h1>
+
+<div class="characters">
+	{#each $characters as character}
+        <Item>
+            <img slot='img' class='character__img' src={character.image} alt={character.titnamele}>
+            <h2 slot="name">{character.name}</h2>
+            <span slot="status">Status: {character.status}</span>
+            <span slot="species">Species:{character.species}</span>
+            <span slot="gender">Gender: {character.gender}</span>
+        </Item>
+	{:else}
+		<!-- this block renders when characters.length === 0 -->
+		<p>loading...</p>
+	{/each}
+</div>
